4c595fa435c49e3e47ae0e841861d631b770f763
Seungyou <kseungyou@gmail.com>
2022-06-28 18:23:14 +0900

    4-4 first solution, recursive

--- implementation/4_4.py
+++ implementation/4_4.py
@@ -1,6 +1,7 @@
 #Given n x m map and player's current position,
 #return the number of spaces the player moved before stopping
 
+
 n, m = map(int, input().split())
 x, y, face = map(int, input().split())
 
@@ -10,7 +11,42 @@ world = [list(map(int, input().split())) for _ in range(n)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-print(n, m)
-print(x, y, face)
-print(world)
+res = 1
+visited = set()
 
+def turnleft(face):
+    face = face - 1
+    if face < 0:
+        face = 3
+    return face
+
+def walkback(x, y, face):
+    face += 2
+    if face > 3:
+        face = face % 2
+    return x + dx[face], y + dy[face]
+
+#Given x, y is space of the map 
+#return true if player can move there
+def canmove(x, y):
+    return world[x][y] == 0 and (x, y) not in visited
+
+def move(x, y, face, strikes = 0):
+    global res, visited
+    visited.add((x, y))
+    face = turnleft(face)
+    if canmove(x + dx[face], y + dy[face]):
+        res += 1
+        move(x + dx[face], y + dy[face], face)
+    else:
+        strikes += 1
+        if strikes > 3:
+            x, y = walkback(x, y, face)
+            if world[x][y] == 0:
+                move(x, y, face)
+            else:
+                return None
+        move(x, y, face, strikes)
+
+move(x, y, face)
+print(res)
