5834211674920b0ddedb5da112b7a20d4c1c01b7
Seungyou <kseungyou@gmail.com>
2022-06-29 14:51:55 +0900

    Q5_10 done

--- dfs_bfs/Q5_10.py
+++ dfs_bfs/Q5_10.py
@@ -6,3 +6,26 @@ m, n = map(int,f.readline().rstrip().split())
 graph = []
 for _ in range(m):
     graph.append(list(map(int, f.readline().rstrip())))
+
+print(f.readline().rstrip())
+
+def dfs(x, y):
+    if x < 0 or y < 0 or x > m - 1 or y > n - 1:
+        return False
+    if graph[x][y] == 1:
+        return False
+    else:
+        graph[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x + 1, y)
+        dfs(x, y - 1)
+        dfs(x, y + 1)
+    return True
+
+blocks = 0
+for i in range(m):
+    for j in range(n):
+        if dfs(i, j):
+            blocks += 1
+
+print("Result:", blocks)
